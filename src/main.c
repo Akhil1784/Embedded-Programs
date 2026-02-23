1da5b6b98185c681db33d9dda34b886af57bf98f
Akhil1784 <akhil.ajitha@trenser.com>
2026-02-23 11:10:02 +0530

    Review comments

--- src/main.c
+++ src/main.c
@@ -4,14 +4,13 @@
 //*****************************************************************************
 // 
 // File    : main.c
-// Summary : Health monitor simulation . 
+// Summary : Health monitor simulation. 
 // 
 //*****************************************************************************
 
 //******************************* Include Files *******************************
 #include <stdio.h>
 #include "platformTypes.h"
-//******************************* Include Files *******************************
 
 //***************************** Global Constants ******************************
 
@@ -22,10 +21,16 @@
 #define SENSOR_VAL_REPEATED  (50U)
 #define AVG_SAMPLE_COUNT     (2U)
 #define LOOP_LIMIT_EXTENDED  (5U)
+#define TRUE                 ((bool)1)
+#define FALSE                ((bool)0)
+
+//***************************** Local Function ********************************
+static bool AddReading(uint32 ulValue);
+static float GetAverage(uint8 ucCount);
 
 //***************************** Local Variables *******************************
-static int16 s_anSensorReading[BUFFER_SIZE] = {0}; 
-static uint8 s_ucHistoryIndex = 0U;
+static int16 spSensorReading[BUFFER_SIZE] = {0}; 
+static uint8 ucHistoryIndex = 0U;
 
 //***************************** Global Variables ******************************
 
@@ -35,19 +40,17 @@ static uint8 s_ucHistoryIndex = 0U;
 // Purpose  : Adds a new sensor reading to the internal history buffer while 
 //            preventing buffer overflow. 
 // Inputs   : ulValue - The 32-bit unsigned sensor reading to be stored.
-// Outputs  : Updates the s_anSensorReading array with the new value and 
-//            increments the s_ucHistoryIndex.
-// Return   : bool - TRUE if the reading was successfully added to the buffer; 
-//            FALSE if the buffer is full. 
+// Outputs  : Updates the spSensorReading array and increments ucHistoryIndex.
+// Return   : blStatus - TRUE if successful; FALSE if buffer is full. 
 //*****************************************************************************
-bool AddReading(uint32 ulValue)
+static bool AddReading(uint32 ulValue)
 {
     bool blStatus = FALSE;
 
-    if (s_ucHistoryIndex < BUFFER_SIZE)
+    if (ucHistoryIndex < BUFFER_SIZE)
     {
-        s_anSensorReading[s_ucHistoryIndex] = (int16)ulValue;
-        s_ucHistoryIndex++;
+        spSensorReading[ucHistoryIndex] = (int16)ulValue;
+        ucHistoryIndex++;
         blStatus = TRUE;
     }
     else
@@ -59,23 +62,21 @@ bool AddReading(uint32 ulValue)
 }
 
 //******************************.FUNCTION_HEADER.******************************
-// Purpose  : Calculates the mathematical average of a specified number of 
-//            previous sensor readings from the history buffer.
+// Purpose  : Calculates the average of specified previous sensor readings.
 // Inputs   : ucCount - The number of historical samples to include. 
 // Outputs  : None. 
-// Return   : float - The calculated average of the requested samples. Returns 
-//            0.0f if the count is invalid.
+// Return   : fSum - The calculated average. Returns 0.0f if count is invalid.
 //*****************************************************************************
-float GetAverage(uint8 ucCount)
+static float GetAverage(uint8 ucCount)
 {
-    float fSum   = 0.0f;
-    uint8 ucIIdx = 0U;
+    float fSum    = 0.0f;
+    uint8 ucIdx   = 0U; 
 
-    if ((0U < ucCount) && (ucCount <= s_ucHistoryIndex))
+    if ((0U < ucCount) && (ucCount <= ucHistoryIndex))
     {
-        for (ucIIdx = 0U; ucIIdx < ucCount; ucIIdx++)
+        for (ucIdx = 0U; ucIdx < ucCount; ucIdx++)
         {
-            fSum += (float)s_anSensorReading[ucIIdx];
+            fSum += (float)spSensorReading[ucIdx];
         }
         fSum = fSum / (float)ucCount;
     }
@@ -92,15 +93,17 @@ float GetAverage(uint8 ucCount)
 int main(void)
 {
     int32 lInitialVal = 0; 
-    const int32 *plSafePtr = &lInitialVal; 
+    const int32 * plSafePtr = &lInitialVal; 
+    uint8 ucJIdx = 0U; 
 
     printf("--- Health Monitor Simulation ---\n\n");  
     (void)AddReading(SENSOR_VAL_INITIAL);
     (void)AddReading(SENSOR_VAL_SECONDARY);   
+    
     float fAvg = GetAverage(AVG_SAMPLE_COUNT);
     printf("Calculated Avg: %.2f\n", (double)fAvg);
 
-    for (uint8 ucJIdx = 0U; ucJIdx < LOOP_LIMIT_EXTENDED; ucJIdx++)
+    for (ucJIdx = 0U; ucJIdx < LOOP_LIMIT_EXTENDED; ucJIdx++)
     {
         (void)AddReading(SENSOR_VAL_REPEATED); 
     }
@@ -109,5 +112,3 @@ int main(void)
 
     return 0;
 }
-
-//******************************** End of File ********************************
